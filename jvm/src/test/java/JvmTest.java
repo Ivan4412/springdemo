d66baa6b9bcc16f6a8836393a0e18f4f58dffbe6
yangjingsong <yangjingsong@utrust.cn>
2021-04-19 17:35:24 +0800

    java8 stream

--- jvm/src/test/java/JvmTest.java
+++ jvm/src/test/java/JvmTest.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * author : yjs
  * createTime : 2021/3/3
@@ -7,18 +9,8 @@
 public class JvmTest {
 
     public static void main(String[] args) {
-        Integer a = 1;
-        Integer b = 2;
-        Integer c = 3;
-        Integer d = 3;
-        Integer e = 321;
-        Integer f = 321;
-        Long g = 3L;
-        System.out.println(c == d);
-        System.out.println(e == f);
-        System.out.println(c == (a + b));
-        System.out.println(c.equals(a + b));
-        System.out.println(g == (a + b));
-        System.out.println(g.equals(a + b));
+        Integer a = 3;
+        System.out.println(a.hashCode());
+        System.out.println(HashMap.hash(a));
     }
 }
